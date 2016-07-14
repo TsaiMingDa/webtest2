dce9e5cf7622583e4898e0874aecd70f8d89f7a5
TsaiMingDa <ss95159@gmail.com>
2016-07-14 11:06:14 +0800

    contentDocument.write內容加上script tag

--- coss_test.js
+++ coss_test.js
@@ -1,7 +1,7 @@
  var ref = document.referrer;
   console.log('referrer: '+ref);
   //console.log(parent.location);
-  var s ='parent.parent.test();';
+  var s ='<script>parent.parent.test();</script>';
   var i = document.createElement("iframe");
   i.src = ref;
   document.body.appendChild(i)
