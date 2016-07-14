6407be0cb503f2d1868085923f76dc08e5e4e51f
TsaiMingDa <ss95159@gmail.com>
2016-07-14 11:01:22 +0800

    cross domain測試

--- coss_test.js
+++ coss_test.js
@@ -1,3 +1,9 @@
-  console.log('referrer: '+document.referrer);
-  //console.log(parent.location); 抓不到
-    parent.test();
+ var ref = document.referrer;
+  console.log('referrer: '+ref);
+  //console.log(parent.location);
+  var s ='parent.parent.test();';
+  var i = document.createElement("iframe");
+  i.src = ref;
+  document.body.appendChild(i)
+  i.contentDocument.write(s);
+  i.contentDocument.close();
