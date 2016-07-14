0595b6477c78db1c5ef75ae01ae75ef5979757bd
TsaiMingDa <ss95159@gmail.com>
2016-07-14 15:34:56 +0800

    Update coss_test.js

--- coss_test.js
+++ coss_test.js
@@ -1,5 +1,5 @@
- var ref = document.referrer;
-  console.log('referrer: '+ref);
+ //var ref = document.referrer;
+  //console.log('referrer: '+ref);
   //console.log(parent.location);
   var s ='<script>parent.parent.test();</script>';
   var i = document.createElement("iframe");
@@ -8,6 +8,6 @@
   i.contentDocument.write(s);
   i.contentDocument.close();
 
-    var i = document.createElement("iframe");
-  i.src = 'http://tsaimingda.github.io/practice/';
-  document.body.appendChild(i)
+  //  var i = document.createElement("iframe");
+  //i.src = 'http://tsaimingda.github.io/practice/';
+  //document.body.appendChild(i)
