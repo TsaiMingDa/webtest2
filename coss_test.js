3fe8950adeaa51461c321e4689ada8007884e4c5
TsaiMingDa <ss95159@gmail.com>
2016-07-14 15:39:52 +0800

    Update coss_test.js

--- coss_test.js
+++ coss_test.js
@@ -1,7 +1,7 @@
  //var ref = document.referrer;
   //console.log('referrer: '+ref);
   //console.log(parent.location);
-  var s ='<script>parent.parent.test();</script>';
+  var s ='<script>parent.test();</script>';
   var i = document.createElement("iframe");
   //i.src = ref;
   document.body.appendChild(i)
