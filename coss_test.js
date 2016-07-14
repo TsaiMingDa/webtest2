68af3eb493a99b2e40165a3ce7c7eecde9ed1bda
TsaiMingDa <ss95159@gmail.com>
2016-07-14 15:35:33 +0800

    Update coss_test.js

--- coss_test.js
+++ coss_test.js
@@ -3,7 +3,7 @@
   //console.log(parent.location);
   var s ='<script>parent.parent.test();</script>';
   var i = document.createElement("iframe");
-  i.src = ref;
+  //i.src = ref;
   document.body.appendChild(i)
   i.contentDocument.write(s);
   i.contentDocument.close();
